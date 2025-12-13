c45b0011f497e7d7b594266deb308a1f7fb9b756
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-12-13 21:25:51 +0500

    Add initial project files including configuration, requirements, and email template

---
+++ apps/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class config:
+    SMTP_SERVER = "smtp.gmail.com"
+    SMTP_PORT = 587
+
+    EMAIL = os.getenv("EMAIL")
+    PASSWORD = os.getenv("EMAIL_PASSWORD")
